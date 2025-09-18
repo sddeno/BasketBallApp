a7f8259a45eaa1045021762569aa82761f67f822
Shubham Deshmukh <shubhamdeshmukh69@gmail.com>
2025-09-18 20:11:35 +0530

    Initial Commit

---
+++ BasketBallApp/BasketBallAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  BasketBallAppApp.swift
+//  BasketBallApp
+//
+//  Created by Shubham Deshmukh on 18/09/25.
+//
+
+import SwiftUI
+
+@main
+struct BasketBallAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
